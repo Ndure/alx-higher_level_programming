ea3c9ebf7d5c3ec1df25b85b3edd0366bedf46ee
Ndure <mainanicholas53@gmail.com>
2023-04-27 11:36:24 +0000

    correction

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,15 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-const file = process.argv[2];
 
-fs.readFile(file, 'utf-8', function (err, data) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', (error, data) => {
+  if (error) {
+    console.error(error);
+    return;
   }
+  console.log(data);
 });
+
+
+
